1d84c7476454e5f9aae8f7699197e112135311b9
shirleynoliveiraa <shirley_nill@hotmail.com>
2022-05-11 19:32:43 -0300

    commit da base inicial do app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+
+import Container from './components/layout/Container';
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+      <Container customClass="min-height">
+        <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/company" element={<Company/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/newproject" element={<NewProject/>} />
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
   );
 }
 
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <h1>Contato</h1>
+  )
+}
+
+export default Contact
