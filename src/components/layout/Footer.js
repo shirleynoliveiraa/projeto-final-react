743a5c69072cc913e51639256a536dd9f53eef5f
shirleynoliveiraa <shirley_nill@hotmail.com>
2022-05-11 20:20:23 -0300

    incluindo e estilizando o menu e páginas de navegação

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FaFacebook, FaInstagram, FaLinkedin, FaGithub} from 'react-icons/fa';
+import styles from './Footer.module.css';
+
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <ul className={styles.social_list}>
+        <li><FaFacebook /></li>
+        <li><FaInstagram /></li>
+        <li><FaLinkedin /></li>
+        <li><FaGithub /></li>
+      </ul>
+      <p className={styles.copy_right}><span>Costs</span> &copy; 2022</p>
+    </footer>
+  )
+}
+
+export default Footer
