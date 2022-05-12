fe61d2ef16b3cb64979a0041f754790dbf9a6c65
shirleynoliveiraa <shirley_nill@hotmail.com>
2022-05-12 16:12:21 -0300

    criar banco de dados, fzr conexao com API

---
+++ src/components/layout/Message.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './Message.module.css';
+
+
+const Message = ({ type, msg }) => {
+  const [visible, setVisible] = React.useState(false);
+
+  React.useEffect(() => {
+    if (!msg) {
+      setVisible(false);
+      return;
+    }
+
+    setVisible(true);
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [msg]);
+
+  return (
+    <>
+      {visible && (
+        <div className={`${styles.message} ${styles[type]}`}>{msg}</div>
+      )}
+    </>
+  );
+};
+
+export default Message
