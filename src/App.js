a0efb153057639d234c1d7028c1c5106c1c1fe61
shirleynoliveiraa <shirley_nill@hotmail.com>
2022-05-12 21:49:16 -0300

    projeto final completo

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
 
 import Container from './components/layout/Container';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+
+
 
 function App() {
   return (
@@ -20,6 +23,7 @@ function App() {
         <Route path="/company" element={<Company/>} />
         <Route path="/contact" element={<Contact/>} />
         <Route path="/newproject" element={<NewProject/>} />
+        <Route path="/projects/:id" element={<Project />} />
         </Routes>
       </Container>
       <Footer />
